86063b98639d067a7ee83f220b2de6565b4e2009
Sebastian Davidsson Björn <Sebastian.davidsson.bjorn@gmail.com>
2026-02-12 15:56:07 +0100

    uppdaterat reset.sh och apply.sh

--- db/scripts/apply.sh
+++ db/scripts/apply.sh
@@ -6,7 +6,7 @@ source "$(dirname "$0")/config.env"
 echo "Migrerar till '$DB'."
 for f in $(ls -l ../migrations/*.sql 2>/dev/null | sort); do
     echo "Running $f"
-    mysql -u "$USER -p"$PASS" -h "$HOST" "$DB" < "$f"
+    mysql -u "$USER" -h "$HOST" "$DB" < "$f"
 done
 
 echo "Slutförd."
--- db/scripts/reset.sh
+++ db/scripts/reset.sh
@@ -5,18 +5,18 @@ set -e
 source "$(dirname "$0")/config.env
 
 echo "Återskapar '$DB'."
-mysql -u "$USER" -p"$PASS" -h "$HOST" -e
+mysql -u "$USER" -h "$HOST" -e
 
 echo "Init script."
 for f in $(ls -l ../init/*.sql | sort); do
     echo "Kör $f"
-    mysql -u "$USER" -p"$PASS" -h "$HOST" "$DB" < "$f"
+    mysql -u "$USER" -h "$HOST" "$DB" < "$f"
 done
 
 echo "Migrerar"
 for f in $(ls -l ../migrations/*.sql 2>/dev/null | sort); do
     echo "Kör $f"
-    mysql -u "$USER" -p"$PASS" -h "$HOST" "$DB" < "$f"
+    mysql -u "$USER" -h "$HOST" "$DB" < "$f"
 done
 
 echo "Databas återställd"
