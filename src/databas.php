51c239ca3c2eb3ad9399dd14710b4a58ffa83e0f
Sebastian Davidsson Björn <Sebastian.davidsson.bjorn@gmail.com>
2026-03-02 14:38:06 +0100

    databas uppdatering

--- src/databas.php
+++ src/databas.php
@@ -1,8 +1,7 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-$pdo = new PDO(
-	"mysql:host=localhost;
-	dbname=hyrabil;
-	charset=utf8", "bjrrn", "666");
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+<?php 
+$servername = "sql307.infinityfree.com"; 
+$username = "if0_41069409"; 
+$password = "H4lxKbPj1O5"; 
+$dbname = "f0_41069409_databas"; 
+$conn = new mysqli($servername, $username, $password, $dbname); 
+if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); } ?>
