6413dc20c52a04e30d740e708cb87060468246e1
Roha-riri <roha.alrajeh.se@gmail.com>
2026-03-15 20:59:20 +0100

    ändrade på js så den matchar med api, samt uppdaterat på att user kan skriva tel

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,6 @@
  window.addEventListener('DOMContentLoaded', function() {
 
-    fetch('../public/api.php?action=getUserDetails')
+    fetch('../public/api2.php?action=getUserDetails')
         .then(response => {
             if (!response.ok) {
                 throw new Error("User not logged in");
@@ -11,11 +11,12 @@
             if (result.success) {
                 const user = result.data;
 
-                document.getElementById('user-full-name').textContent = user.first_name + " " + user.last_name;
+                document.getElementById('user-full-name').textContent = user.full_name;
 
                 document.getElementById('user-email-address').textContent = user.email;
-
-                document.getElementById('user-phone-number').textContent = user.phone_number ? user.phone_number : "Not added yet";
+                const phoneInput = document.getElementById('user-phone-number');
+                phoneInput.value = user.phone ? user.phone : "";
+                document.getElementById('user-phone-number').textContent = user.phone ? user.phone : "Not added yet";
             } else {
                 window.location.href = '../html/bil_login.html';
                 }
@@ -24,9 +25,35 @@
             console.log("Error:", error);
             window.location.href = '../html/bil_login.html';
             });
+            
+        document.getElementById('save-phone').addEventListener('click', function () {
+            const Phone = document.getElementById('user-phone-number').value.trim();
+            let phonePattern = /^[0-9]+$/;
+            phone = phone.replace(/[^0-9]/g, '');
+                if (phone.length < 7 || phone.length > 15) {
+                    alert("Please enter a valid phone number (7-15 digits).");
+                    return;
+                }
+            
+            fetch('../public/api2.php?action=updatePhone', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ phone: phone })
+            })
+            .then(response => response.json())
+            .then(result => {
+                if (result.success) {
+                    alert("Phone number updated successfully!");
+                } else {
+                    alert("Failed to update phone number.");
+                }
+            });
+        });
 
     document.getElementById('logout-btn-profile').addEventListener('click', function() {
-        fetch('../public/api.php?action=logout')
+        fetch('../public/api2.php?action=logout')
             .then(() => {
                 window.location.href = '../html/bil_login.html';
             });
